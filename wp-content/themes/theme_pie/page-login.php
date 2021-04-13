5633b8b88b780a799c5a3f78e814ae20ab10234a
Samy Mahmoudi <samy.mahmoudi@edu.devinci.fr>
2021-04-13 16:20:50 +0200

    Add plugin ultimate member && page login

---
+++ wp-content/themes/theme_pie/page-login.php
@@ -0,0 +1,15 @@
+<?php get_header() ?>
+
+<section class="section section-login">
+    <article>
+        <h1><?php the_title() ?></h1>
+        <?php the_content() ?>
+        <?php echo apply_shortcodes('[ultimatemember form_id="128"]'); ?>  
+    </article>
+    <aside>
+        <img src="http://localhost/Paper_Industry_Events/wp-content/uploads/2021/04/sign-in.png" alt="women_in_office">
+    </aside>
+</section>
+
+
+<?php get_footer() ?>
