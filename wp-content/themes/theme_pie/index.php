7ba64640f8c8054780582e0446aa3f4c53c3ab9d
Samy Mahmoudi <samy.mahmoudi@edu.devinci.fr>
2021-04-17 17:06:45 +0200

    Article card design & responsive done

--- wp-content/themes/theme_pie/index.php
+++ wp-content/themes/theme_pie/index.php
@@ -1,35 +1,32 @@
-<?php
-	get_header();
-?>
-		<?php if ( have_posts() ) : ?>
+<?php get_header(); ?>
+		
+	<?php if ( have_posts() ) : ?>
 
-			<section>
-				<h1>Blog</h1>
-				<section class="section-blog">
-			<?php 
-				while ( have_posts() ) : the_post(); ?>
-					<article class="card-article">
-						<div class="card-article-img">
-							<?php the_post_thumbnail() ?>
-						</div>
-						<div class="card-article-header">
-							<h3><?php the_title(); ?></h3>
-								<?php $article_data = substr(get_the_content(), 0, 100); 
-									echo $article_data ; 
-								?>...
+		<section>
+			<h1>Blog</h1>
+			<section class="section-blog">
+			<?php while ( have_posts() ) : the_post(); ?>
 						
-							<div>
-								<span><?php the_time('d/m/Y'); ?></span>
-								<a class="card-article-link" href="<?= the_permalink() ?>" title="Article : <?php the_title(); ?>">Read the article</a>
-							</div>
+				<article class="card-article">
+					<div class="card-article-img">
+						<?php the_post_thumbnail() ?>
+					</div>
+					<div class="card-article-header">
+						<h3><?php the_title(); ?></h3>
+						<?php $article_data = substr(get_the_content(), 0, 100); 
+							echo $article_data ; 
+						?>...
+						<div>
+							<span><?php the_time('d/m/Y'); ?></span>
+							<a class="card-article-link" href="<?= the_permalink() ?>" title="Article : <?php the_title(); ?>">Read the article</a>
 						</div>
-						
-					</article>
+					</div>
+				</article>
+
 			<?php endwhile; ?>
-
-		<?php endif; ?>
-				</section>
 			</section>
-	
+		</section>
+
+	<?php endif; ?>
 
 <?php get_footer();
