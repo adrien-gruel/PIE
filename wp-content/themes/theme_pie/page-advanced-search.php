45452cc1f686f56d1990a65b22bb377aeb177dc2
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-04-19 09:51:28 +0200

    Finished search feature

--- wp-content/themes/theme_pie/page-advanced-search.php
+++ wp-content/themes/theme_pie/page-advanced-search.php
@@ -16,21 +16,22 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+        <div class="title-banner">
+        <h1>Advanced search</h1>
+        </div>
 
         <form method="GET" action="advanced-search">
         <h2>Where ?</h2>
         <div class="search_where">
             <p>
-                <label for="country">Country</label>
-                <input type="text" id="search_country" name="search_country" />
+                <input placeholder="Country" type="text" id="search_country" name="search_country" />
             </p>
             <p>
-                <label for="city">City</label>
-                <input type="text" id="search_city" name="search_city" />
+                <input placeholder="City" type="text" id="search_city" name="search_city" />
             </p>
             <p>
-                <label for="search_accessibility">Accessibility</label>
                 <select id="search_accessibility" name="search_accessibility">
+                    <option value="select">Accessibility</option>
                     <option value="physical">Physical</option>
                     <option value="virtual">Virtual</option>
                     <option value="hybrid">Hybrid</option>
@@ -41,40 +42,24 @@ get_header();
         <h2>When ?</h2>
         <div class="search_when">
             <p>
-                <label for="search_start_date">From</label>
-                <input type="date" id="search_start_date" name="search_start_date" />
+                <input placeholder="From" type="date" id="search_start_date" name="search_start_date" />
             </p>
             <p>
-                <label for="search_end_date">To</label>
-                <input type="date" id="search_end_date" name="search_end_date" />
-            </p>
-            <p>
-                <label for="search_accessibility">Accessibility</label>
-                <select id="search_accessibility" name="search_accessibility">
-                    <option value="physical">Physical</option>
-                    <option value="virtual">Virtual</option>
-                    <option value="hybrid">Hybrid</option>
-                </select>
+                <input placeholder="To" type="date" id="search_end_date" name="search_end_date" />
             </p>
         </div>
 
         <h2>What ?</h2>
         <div class="search_what">
             <p>
-                <label for="search_organizer">Organizer</label>
-                <input type="date" id="search_organizer" name="search_organizer" />
+                <input placeholder="Fees" type="number" id="search_fees" name="search_fees" />
             </p>
             <p>
-                <label for="search_fees">Fees</label>
-                <input type="number" id="search_fees" name="search_fees" />
+                <input placeholder="language" type="text" id="search_language" name="search_language" />
             </p>
             <p>
-                <label for="search_language">Language</label>
-                <input type="text" id="search_language" name="search_language" />
-            </p>
-            <p>
-                <label for="search_type">Type</label>
-                <select id="search_type" name="search_type">
+                <select id="search_event_types" name="search_event_types">
+                <option value="select">Select type</option>
                     <?php foreach ( get_event_listing_types() as $types ) : ?>
                         <option value="<?php echo esc_attr( $types->term_id ); ?>"><?php echo esc_html( $types->name ); ?></option>
                     <?php endforeach; ?>
@@ -86,25 +71,48 @@ get_header();
             </p>
         </form>
 
+
         <?php 
+        $custom_city = !empty($_GET['search_city']) ? array('key' => '_city', 'value' => $_GET['search_city']) : array();
+        $custom_country= !empty($_GET['search_country']) ? array('key' => '_country', 'value' => $_GET['search_country']) : array();
+        $custom_accessibility= $_GET['search_accessibility'] != 'select' ? array('key' => '_accessibility', 'value' => $_GET['search_accessibility']) : array();
+        $custom_start_date= !empty($_GET['search_start_date']) ? array('key' => '_start_date', 'value' => $_GET['search_start_date'], 'compare' => '>=', 'type' => 'DATE') : array();
+        $custom_end_date= !empty($_GET['search_end_date']) ? array('key' => '_end_date', 'value' => $_GET['search_end_date'], 'compare' => '<=', 'type' => 'DATE') : array();
+        $custom_fees= !empty($_GET['search_fees']) ? array('key' => '_fees', 'value' => $_GET['search_fees'], 'compare' => '<=', 'type' => 'numeric') : array();
+        $custom_language= !empty($_GET['search_language']) ? array('key' => '_language', 'value' => $_GET['search_language']) : array();
+
+
                 $args = array( 
                     'post_type' => 'event_listing', 
-                    'meta_key'     => '_city',
-                    'meta_value'   => $_GET['search_city'],
-                    'meta_key' => "_country",
-                    'meta_value' => $_GET['search_country']
+                    'meta_query' => array(
+                        $custom_city,
+                        $custom_country,
+                        $custom_accessibility,
+                        $custom_start_date,
+                        $custom_end_date,
+                        $custom_fees,
+                        $custom_language,
+                    ),
+
                  );
+                
                 $the_query = new WP_Query( $args ); 
+
             ?>
             
             <?php if ( $the_query->have_posts() ) : ?>
             <?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
-            $post = get_post(the_ID())?>
-            <article data-aos="zoom-in-up">
-                <h3><?php the_title() ?></h3>
-                <p><?php echo $post->_city ?></p>
-            </article>
+            $type= get_event_type();
+            if($type[0]->term_id == $_GET['search_event_types'] || $_GET['search_event_types'] === "select"):
+                ?>
+                <article data-aos="zoom-in-up">
+                    <h3><?php the_title() ?></h3>
+                    <p><?php echo $post->_city ?></p>
+                    <?php display_event_type() ?>
+                    
+                </article>
             <?php 
+                endif;
                 endwhile; 
                 wp_reset_postdata(); 
             ?>
