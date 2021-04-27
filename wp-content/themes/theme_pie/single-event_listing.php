e007dea1e4e64f9daa07fbfc28dd10d610ac72f7
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-04-27 14:52:45 +0200

    Fix single event page informations

--- wp-content/themes/theme_pie/single-event_listing.php
+++ wp-content/themes/theme_pie/single-event_listing.php
@@ -18,10 +18,10 @@ $event = $post;
                 <div class="Information">  
                     <p>Type: <?php display_event_type() ?></p>
                     <?php display_event_category() ?>
-                    <p>Start Date: <?php echo date("d", $eventDate)?> / <?php echo date("m", $eventDate)?> / <?php echo date("y", $eventDate)?></p>
-                    <p>End Date: <?php echo date("d", $eventDateEnd)?> / <?php echo date("m", $eventDateEnd)?> / <?php echo date("y", $eventDateEnd)?></p>
-                    <p>Main language: <?php echo $post->_language ?></p>
-                    <p>Simultaneous transmission: </p>
+                    <p>Start: <?php echo date("d/m/y", $eventDate) . " | " . get_event_start_time()?></p>
+                    <p>End: <?php echo date("d/m/y", $eventDateEnd) . " | " . get_event_end_time()?></p>
+                    <p>Main language: <?php 
+                    echo ucfirst($post->_language[0]) ?></p>
                     <p>Event webpage:  <?php echo $post->_event_webpage?></p>
                     <p>Contact Us: <?php display_organizer_email()?></p>
                 </div>
@@ -33,7 +33,7 @@ $event = $post;
                     <h2 class="second-headline">DESCRIPTION</h2>
                     <p><?php echo $post->_event_description?></p>
             </div>
-            <h2 class="second-headline">PLACE & VENUE</h2>
+            <h2 class="second-headline place-venue">PLACE & VENUE</h2>
             <div> 
             <div class="single_event_listing-Place"> 
                 <div class="Information"> 
@@ -42,20 +42,23 @@ $event = $post;
                     <p>Address : <?php echo $post->_address ?> </p>
                 </div>
                 <div class="Information"> 
-                    <p>Online: <?php echo $post->_event_online?></p>
-                    <p>Access: <?php echo $post->_access ?></p>
+                    <p>Access: <?php echo ucfirst($post->_access) ?></p>
                     <p>Fees: <?php echo $post->_fees ?></p>
                 </div>
             </div>
             <h2 class="second-headline">EVENT DETAILS</h2>
             <div class="single_event_listing"> 
                 <div class="Information"> 
-                    <p>Topics: <?php echo $post->_topics?></p>
-                    <p>Products Topics: <?php echo $post->_products_topics?></p>
-                    <p>Technical Topics: <?php echo $post->_technical_topics?></p>
+                    <?php $business = ucfirst(str_replace('_', ' ', $post->_business_topics));
+                          $products = ucfirst(str_replace('_', ' ', $post->_products_topics));
+                          $technical = ucfirst(str_replace('_', ' ', $post->_technical_topics));
+                          $digital = ucfirst(str_replace('_', ' ', $post->_digital_platform))?>
+                    <p>Business Topics: <?php echo $business?></p>
+                    <p>Products Topics: <?php echo $products ?></p>
+                    <p>Technical Topics: <?php echo $technical?></p>
                     <p>List of exhibitors: <?php echo $post->_exhibitors?></p>
                     <p>Programme: <?php echo $post->_programme?></p>
-                    <p>Digital platform: <?php echo $post->_digital_platform?></p>
+                    <p>Digital platform: <?php echo $digital?></p>
                 </div>
                 <div class="image-logo">
                     <img src="<?php echo get_event_thumbnail() ?>" alt="">
