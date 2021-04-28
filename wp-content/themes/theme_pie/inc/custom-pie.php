ba706dc801646b1ab2434d1c508849c50fb36b23
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-04-28 16:10:34 +0200

    Update all plugins / Fixing single event listing

--- wp-content/themes/theme_pie/inc/custom-pie.php
+++ wp-content/themes/theme_pie/inc/custom-pie.php
@@ -4,6 +4,7 @@
  */
 function custom_pie_scripts() {
 	wp_enqueue_script( 'menuResponsive', get_stylesheet_directory_uri() . '/js/menuResponsive.js', array(), _S_VERSION, true);
+    wp_localize_script( 'menuResponsive', 'adminAjax', admin_url( 'admin-ajax.php' ) );
 }
 add_action( 'wp_enqueue_scripts', 'custom_pie_scripts');
 
@@ -14,3 +15,118 @@ register_nav_menus(
     )
 );
 
+add_action( 'wp_ajax_search_ajax', 'search_ajax' );
+add_action( 'wp_ajax_nopriv_search_ajax', 'search_ajax');
+
+function search_ajax(){
+    $data = $_GET['data'];
+    $custom_city = !empty($data[2]['value']) ? array('key' => '_city', 'value' => $data[2]['value']) : array();
+    $custom_country= !empty($data[1]['value']) ? array('key' => '_country', 'value' => $data[1]['value']) : array();
+    $custom_start_date= !empty($data[3]['value']) ? array('key' => '_event_start_date', 'value' => $data[3]['value'], 'compare' => '>=', 'type' => 'DATE') : array();
+    $custom_end_date= !empty($data[4]['value']) ? array('key' => '_event_end_date', 'value' => $data[4]['value'], 'compare' => '<=', 'type' => 'DATE') : array();
+    $custom_fees_start= !empty($data[5]['value']) ? array('key' => '_fees', 'value' => $data[5]['value'], 'type' => 'numeric', 'compare' => '>=') : array();
+    $custom_fees_end= !empty($data[6]['value']) ? array('key' => '_fees', 'value' => $data[6]['value'], 'type' => 'numeric', 'compare' => '<=') : array();
+    $custom_language= $data[7]['value'] != "select" ? array('key' => '_language', 'value' => $data[7]['value'], 'compare' => 'LIKE') : array();
+    $custom_type= $data[8]['value'] != "select" ? array('taxonomy' => 'event_listing_type', 'field' => 'term_id', 'terms' => $data[8]['value']) : array('taxonomy' => 'event_listing_type', 'operator' => 'EXISTS');
+    $args = array( 
+        'post_type' => 'event_listing', 
+        'post_status' => 'publish',
+        'meta_query' => array(
+            $custom_city,
+            $custom_country,
+            $custom_start_date,
+            $custom_end_date,
+            $custom_fees_start,
+            $custom_fees_end,
+            $custom_language,
+        ),
+        'tax_query' => array(
+            $custom_type
+        )
+    );
+
+    $the_query = new WP_Query( $args ); 
+
+
+    ob_start();
+
+    if($the_query->have_posts()){
+        while($the_query->have_posts()){
+            $the_query->the_post();
+            global $post;
+            ?>
+            <div class="wpem-event-box-col wpem-col wpem-col-12 wpem-col-md-6 wpem-col-lg-4">
+                <div class="wpem-event-layout-wrapper">
+                    <div
+                        class="event_listing event-type-appearance-or-signing post-274 type-event_listing status-expired has-post-thumbnail hentry event_listing_type-appearance-or-signing">
+                        <a href="<?php the_permalink() ?>"
+                            class="wpem-event-action-url event-style-color dinner-or-gala">
+                            <div class="wpem-event-banner">
+
+                                <div class="wpem-event-banner-img"
+                                    style="background-image: url('<?php echo get_event_thumbnail() ?>')">
+                                    <div class="wpem-event-date">
+                                        <div class="wpem-event-date-type">
+                                            <div class="wpem-from-date">
+                                                <div class="wpem-date"><?php 
+                                                                $eventDate = strtotime(get_event_start_date());
+                                                                echo date("d", $eventDate);
+                                                            ?></div>
+                                                <div class="wpem-month"><?php echo date("M", $eventDate);?></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <div class="wpem-event-infomation">
+                            <div class="wpem-event-details">
+                                <a href="<?php the_permalink() ?>"
+                                    class="wpem-event-action-url event-style-color dinner-or-gala">
+                                    <div class="wpem-event-title">
+                                        <h3 class="wpem-heading-text"><?php display_event_title() ?></h3>
+                                    </div>
+                                    <div class="wpem-event-date-time">
+                                        <span class="wpem-event-date-time-text">
+                                            <?php display_event_start_date() ?> @ <?php display_event_start_time() ?> -
+                                            <?php display_event_end_date() ?> @ <?php display_event_end_time() ?>
+                                        </span>
+                                    </div>
+                                    <div class="wpem-event-location">
+                                        <span class="wpem-event-location-text">
+                                            <?php echo $post->_country . " | " . $post->_city ?>
+                                        </span>
+                                    </div>
+                                </a>
+                                <div class="wpem-event-type">
+                                    <?php display_event_type() ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        wp_reset_postdata(); 
+    } else {
+        ?>
+        <div class="error-container">
+            <p>No events found</p>
+        </div>
+    <?php 
+    }
+    echo ob_get_clean();
+    wp_die(); 
+}
+
+add_action( 'template_redirect', 'redirect_to_specific_page' );
+
+function redirect_to_specific_page() {
+
+if ( (is_page('your-events') || is_page('create-event'))&& ! is_user_logged_in() ) {
+
+wp_redirect( './login'); 
+  exit;
+    }
+}
