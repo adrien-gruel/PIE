8b70a6b57cf885bfaea0994c87c073e4ee25aa77
Samy Mahmoudi <samy.mahmoudi@edu.devinci.fr>
2021-04-29 15:17:46 +0200

    Edit style ads, advanced search & homepage

--- wp-content/themes/theme_pie/single-post.php
+++ wp-content/themes/theme_pie/single-post.php
@@ -1,24 +1,34 @@
 <?php get_header() ?>
+
+<main id="primary" class="site-main main-waves">
+    
 <section class="section section-single">
-    <div class="top-bot-orange single-top"></div>
-    <div class="container">
-        <article>
-            <h1><?php the_title() ?></h1>
-            <div class="image-mise-en-avant"><?php the_post_thumbnail() ?></div>
-            <p><?php the_content() ?></p>
-            <?php the_post_navigation(
-                    array(
-                        'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'theme_pie' ) . '</span> <span class="nav-title">%title</span>',
-                        'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'theme_pie' ) . '</span> <span class="nav-title">%title</span>',
-                    )
-                );
-            ?>
-        </article>
-    </div>
-    <section>
-        <?php the_ad_group(90); ?>
-    </section>
-</section>
+        
+        <div class="top-bot-orange single-top"></div>
+        <div class="container">
+            <article>
+                <h1><?php the_title() ?></h1>
+                <div class="image-mise-en-avant"><?php the_post_thumbnail() ?></div>
+                <p><?php the_content() ?></p>
+                <?php the_post_navigation(
+                        array(
+                            'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'theme_pie' ) . '</span> <span class="nav-title">%title</span>',
+                            'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'theme_pie' ) . '</span> <span class="nav-title">%title</span>',
+                        )
+                    );
+                ?>
+            </article>
+        </div>
+
+        <section class="section-ads">
+            <section>
+                <?php the_ad_group(90); ?>
+            </section>
+        </section><!-- Section ads bottom article -->
+        
+
+    </section><!-- Section post (article) -->
+</main> 
 
 
 
