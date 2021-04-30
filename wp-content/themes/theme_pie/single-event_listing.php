d967fe1f8f1f7b8591fb08431f3849adc42208c3
Samy Mahmoudi <samy.mahmoudi@edu.devinci.fr>
2021-04-30 11:26:17 +0200

    Edit single post & single event

--- wp-content/themes/theme_pie/single-event_listing.php
+++ wp-content/themes/theme_pie/single-event_listing.php
@@ -15,78 +15,102 @@
     <div class="container">
         
         <article class="event-single">
+            
             <h1><?php the_title() ?></h1>
-            <h2 class="second-headline">INFORMATIONS</h2>
-            <div class="single_event_listing">
-                <div class="Information">  
-                    <p>Type: <?php display_event_type() ?></p>
-                    <?php display_event_category() ?>
-                    <p>Start: <?php echo date("d/m/y", $eventDate) . " | " . get_event_start_time()?></p>
-                    <p>End: <?php echo date("d/m/y", $eventDateEnd) . " | " . get_event_end_time()?></p>
-                    <p>Languages: <?php 
-                    foreach($post->_language as $language){
-                        echo ucfirst($language)." | ";
-                    } ?>
-                    </p>
-                    <p>Event webpage: <a href="<?php echo $post->_event_webpage?>"><?php echo $post->_event_webpage?></a></p>
-                    <p>Contact Us: <?php display_organizer_email()?></p>
-                </div>
-                <div class="image-logo">
-                   <?php display_organizer_logo() ?>
-                </div>
-            </div>
-            <div class="description">
-                    <h2 class="second-headline">DESCRIPTION</h2>
-                    <p><?php echo $post->_event_description?></p>
-            </div>
-            <h2 class="second-headline place-venue">PLACE & VENUE</h2>
-            <div> 
-            <div class="single_event_listing-Place"> 
-                <div class="Information"> 
-                    <p>Country : <?php echo $post->_country ?></p>
-                    <p>City : <?php echo $post->_city ?></p>
-                    <p>Address : <?php echo $post->_address ?> </p>
-                </div>
-                <div class="Information"> 
-                    <p>Access: <?php echo ucfirst($post->_access) ?></p>
-                    <p>Fees: <?php
-                    if($post->_fees != ""){
-                        echo $post->_fees . " $"; 
-                    } else {
-                        echo "Free";
-                    }
-                     ?></p>
-                </div>
-            </div>
-            <section>
-                <?php the_ad_group(89); ?>
+            
+            <section class="section-data-single section-data-single-informations">
+                <article>
+                    <div>  
+                        <h2>INFORMATIONS</h2>
+                        <p><strong>Type:</strong>  <?php display_event_type() ?></p>
+                        <p><strong>Start:</strong>  <?php echo date("d/m/y", $eventDate) . " | " . get_event_start_time()?></p>
+                        <p><strong>End</strong>: <?php echo date("d/m/y", $eventDateEnd) . " | " . get_event_end_time()?></p>
+                        <p><strong>Languages:</strong>
+                            <?php 
+                                foreach($post->_language as $language){
+                                    if($language)
+                                        echo ucfirst($language)." | ";
+                                } 
+                            ?>
+                        </p>
+                        <p><strong>Event webpage:</strong>  <a href="<?php echo $post->_event_webpage?>"><?php echo $post->_event_webpage?></a></p>
+                        <p><strong>Contact Us:</strong> <?php display_organizer_email()?></p>
+                    </div>
+                    <div class="image-logo">
+                        <?php display_organizer_logo() ?>
+                    </div>
+                </article>
+            </section><!-- Informations of an event -->  
+
+            <section class="section-data-single section-data-single-description">
+                <h2>DESCRIPTION</h2>
+                <?php echo $post->_event_description?>
+            </section><!-- Description of an event -->  
+            
+            <section class="section-data-single section-data-single-placeVenue">
+                <h2>PLACE & VENUE</h2>
+                
+                <article class="article-placeVenue">
+                    <div>
+                        <p><strong>Country:</strong>  <?php echo $post->_country ?></p>
+                        <p><strong>City: </strong> <?php echo $post->_city ?></p>
+                        <p><strong>Address: </strong><?php echo $post->_address ?> </p>
+                    </div>
+                    <div>
+                        <p><strong>Access:</strong> <?php echo ucfirst($post->_access) ?></p>
+                        <p><strong>Fees:</strong> 
+                            <?php 
+                                if($post->_fees != ""){
+                                    echo $post->_fees . " $"; 
+                                } else {
+                                    echo "Free";
+                                }
+                            ?>
+                        </p> 
+                    </div>
+                </article>
+                
+                
+            </section><!-- Place & Venue of an event -->  
+
+            <section class="section-ads">
+                <section>
+                    <?php the_ad_group(89); ?>
+                </section>
+            </section> <!-- Group ads of middle event -->
+           
+            <section class="section-data-single section-data-single-eventDetails"> 
+                <article>
+                    <div>  
+                        <h2>EVENT DETAILS</h2>
+                        <?php 
+                            $business = ucfirst(str_replace('_', ' ', $post->_business_topics));
+                            $products = ucfirst(str_replace('_', ' ', $post->_products_topics));
+                            $technical = ucfirst(str_replace('_', ' ', $post->_technical_topics));
+                            $digital = ucfirst(str_replace('_', ' ', $post->_digital_platform))
+                        ?>
+                        <p><strong>Business Topics:</strong> <?php echo $business?></p>
+                        <p><strong>Products Topics:</strong> <?php echo $products ?></p>
+                        <p><strong>Technical Topics:</strong> <?php echo $technical?></p>
+                        <p><strong>List of exhibitors:</strong> <?php echo $post->_exhibitors?></p>
+                        <p><strong>Programme:</strong> <?php echo $post->_programme?></p>
+                        <p><strong>Digital platform:</strong> <?php echo $digital?></p>
+                    </div>
+                    <div class="image-logo">
+                        <img src="<?php echo get_event_thumbnail() ?>" alt="">
+                    </div>
+                </article>   
             </section>
-            <h2 class="second-headline">EVENT DETAILS</h2>
-            <div class="single_event_listing"> 
-                <div class="Information"> 
-                    <?php $business = ucfirst(str_replace('_', ' ', $post->_business_topics));
-                          $products = ucfirst(str_replace('_', ' ', $post->_products_topics));
-                          $technical = ucfirst(str_replace('_', ' ', $post->_technical_topics));
-                          $digital = ucfirst(str_replace('_', ' ', $post->_digital_platform))?>
-                    <p>Business Topics: <?php echo $business?></p>
-                    <p>Products Topics: <?php echo $products ?></p>
-                    <p>Technical Topics: <?php echo $technical?></p>
-                    <p>List of exhibitors: <?php echo $post->_exhibitors?></p>
-                    <p>Programme: <?php echo $post->_programme?></p>
-                    <p>Digital platform: <?php echo $digital?></p>
-                </div>
-                <div class="image-logo">
-                    <img src="<?php echo get_event_thumbnail() ?>" alt="">
-                </div>
-            </div>
+
         </article>
-
     </div>
-    <section>
-        <?php the_ad_group(88); ?>
-    </section>
+
+    <section class="section-ads">
+        <section>
+            <?php the_ad_group(88); ?>
+        </section>
+    </section><!-- Group ads of middle event -->
+
 </section>
-
-
 
 <?php get_footer();
